84addc653e4188c01ee99ebc834507913e0f0616
Nathan Casados <ncasados@live.com>
2024-06-27 17:36:05 -0600

    Adding find or create helldiver module

---
+++ lib/helldivers_bar_bot/discord_consumer/find_or_create_helldiver.ex
@@ -0,0 +1,24 @@
+defmodule HelldiversBarBot.DiscordConsumer.FindOrCreateHelldiver do
+  alias HelldiversBarBot.Helldivers
+  alias HelldiversBarBot.Helldivers.Helldiver
+
+  @spec call(String.t()) :: any()
+  def call(discord_user_id) do
+    case Helldivers.get_helldiver!(discord_id: to_string(discord_user_id)) do
+      # Found Helldiver
+      %Helldiver{
+        messages_sent: messages_sent,
+        wallet: wallet
+      } = helldiver ->
+        Helldivers.update_helldiver(helldiver, %{
+          "messages_sent" => messages_sent + 1,
+          "wallet" => Decimal.add(wallet, "0.25")
+        })
+
+      # Helldiver not found
+      _ ->
+        Nostrum.Api.get_user!(discord_user_id)
+        |> Helldivers.create_helldiver()
+    end
+  end
+end
