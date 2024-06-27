eceb04403e82168681110254eeec75694735936b
Nathan Casados <ncasados@live.com>
2024-06-27 17:35:27 -0600

    Updating discord consumer

--- lib/helldivers_bar_bot/discord_consumer.ex
+++ lib/helldivers_bar_bot/discord_consumer.ex
@@ -1,65 +1,37 @@
 defmodule HelldiversBarBot.DiscordConsumer do
   use Nostrum.Consumer
-  alias HelldiversBarBot.Repo
-  alias HelldiversBarBot.Helldivers.Helldiver
+
+  alias HelldiversBarBot.MagicEightBall
+  alias HelldiversBarBot.DiscordConsumer.FindOrCreateHelldiver
   alias HelldiversBarBot.Helldivers
+  alias HelldiversBarBot.Helldivers.Helldiver
   alias Nostrum.Api
-  alias Nostrum.Struct.Interaction
   alias Nostrum.Struct.ApplicationCommandInteractionData
   alias Nostrum.Struct.Guild.Member, as: GuildMember
+  alias Nostrum.Struct.Interaction
+  alias Nostrum.Struct.Message
+  alias Nostrum.Struct.User
+  alias Nostrum.Struct.WSState
 
-  def handle_event({:MESSAGE_CREATE, msg, _ws_state}) do
-    content = String.downcase(msg.content)
+  @spec handle_event({:MESSAGE_CREATE, Message.t(), WSState.t()}) ::
+          any()
+  def handle_event(
+        {:MESSAGE_CREATE, %Message{author: %User{id: discord_user_id}, content: content} = msg,
+         _ws_state}
+      ) do
+    content = String.downcase(content)
 
     case content do
       "ping!" ->
         Api.create_message(msg.channel_id, "I copy and pasted this code")
 
-      "bot," <> _ ->
-        messages = [
-          "It is certain",
-          "It is decidedly so",
-          "Without a doubt",
-          "Yes definitely",
-          "You may rely on it",
-          "As I see it, yes",
-          "Most likely",
-          "Outlook good",
-          "Yes",
-          "Signs point to yes",
-          "Reply hazy, try again",
-          "Ask again later",
-          "Better not tell you now",
-          "Cannot predict now",
-          "Concentrate and ask again",
-          "Don't count on it",
-          "My reply is no",
-          "My sources say no",
-          "Outlook not so good",
-          "Very doubtful",
-          "oh yeh baby"
-        ]
+      "barkeep," <> _ ->
+        phrases = MagicEightBall.phrases()
 
-        Api.create_message(msg.channel_id, Enum.random(messages))
+        Api.create_message(msg.channel_id, Enum.random(phrases))
 
       _ ->
-        %Nostrum.Struct.Message{
-          author: %Nostrum.Struct.User{
-            id: discord_id
-          }
-        } = msg
-
-        %Helldiver{
-          messages_sent: messages_sent,
-          wallet: wallet
-        } = member = Helldivers.get_helldiver!(discord_id: to_string(discord_id))
-
-        member
-        |> Helldiver.changeset(%{
-          "messages_sent" => messages_sent + 1,
-          "wallet" => Decimal.add(wallet, "0.25")
-        })
-        |> Repo.update!()
+        FindOrCreateHelldiver.call(discord_user_id)
 
         :ignore
     end
