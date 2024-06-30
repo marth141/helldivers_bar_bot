254c53195f7ee4705a5297ca9c7ee081f3eebf6a
Nathan Casados <ncasados@live.com>
2024-06-29 22:35:25 -0600

    Updating test

--- test/helldivers_bar_bot/discord_consumer_test.exs
+++ test/helldivers_bar_bot/discord_consumer_test.exs
@@ -1,11 +1,5 @@
 defmodule HelldiversBarBot.DiscordConsumerTest do
-  alias Nostrum.Struct.WSState
-  alias Nostrum.Struct.User
-  alias Nostrum.Struct.Message
   use HelldiversBarBot.DataCase
-
-  alias HelldiversBarBot.Drinks
-  alias HelldiversBarBot.Drinks.Drink
 
   # Need to write tests
 end
