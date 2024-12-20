643b074e19e7e6f526c7404fd7d4498e0b3866f2
Nathan Casados <ncasados@live.com>
2024-12-19 20:27:46 -0700

    Ignoring consumer start in test environment

--- config/config.exs
+++ config/config.exs
@@ -70,6 +70,8 @@ config :nostrum,
     :message_content
   ]
 
+config :helldivers_bar_bot, env: config_env()
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
--- lib/helldivers_bar_bot/application.ex
+++ lib/helldivers_bar_bot/application.ex
@@ -7,20 +7,21 @@ defmodule HelldiversBarBot.Application do
 
   @impl true
   def start(_type, _args) do
-    children = [
-      HelldiversBarBotWeb.Telemetry,
-      HelldiversBarBot.Repo,
-      {DNSCluster,
-       query: Application.get_env(:helldivers_bar_bot, :dns_cluster_query) || :ignore},
-      {Phoenix.PubSub, name: HelldiversBarBot.PubSub},
-      # Start the Finch HTTP client for sending emails
-      {Finch, name: HelldiversBarBot.Finch},
-      # Start a worker by calling: HelldiversBarBot.Worker.start_link(arg)
-      # {HelldiversBarBot.Worker, arg},
-      # Start to serve requests, typically the last entry
-      HelldiversBarBotWeb.Endpoint,
-      HelldiversBarBot.DiscordConsumer
-    ]
+    children =
+      [
+        HelldiversBarBotWeb.Telemetry,
+        HelldiversBarBot.Repo,
+        {DNSCluster,
+         query: Application.get_env(:helldivers_bar_bot, :dns_cluster_query) || :ignore},
+        {Phoenix.PubSub, name: HelldiversBarBot.PubSub},
+        # Start the Finch HTTP client for sending emails
+        {Finch, name: HelldiversBarBot.Finch},
+        # Start a worker by calling: HelldiversBarBot.Worker.start_link(arg)
+        # {HelldiversBarBot.Worker, arg},
+        # Start to serve requests, typically the last entry
+        HelldiversBarBotWeb.Endpoint
+      ]
+      |> maybe_add_discord_consumer()
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
@@ -34,5 +35,12 @@ defmodule HelldiversBarBot.Application do
   def config_change(changed, _new, removed) do
     HelldiversBarBotWeb.Endpoint.config_change(changed, removed)
     :ok
+  end
+
+  defp maybe_add_discord_consumer(children) do
+    case Application.get_env(:helldivers_bar_bot, :env) do
+      :test -> children
+      _anything_else -> children ++ [HelldiversBarBot.DiscordConsumer]
+    end
   end
 end
